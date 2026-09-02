5db18c89096a760e99872a83486541cdeb8b8a49
David O'Brien <156262935+davidob05@users.noreply.github.com>
2026-09-03 00:18:10 +0100

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,44 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        
+        n1 = len(s1)
+        n2 = len(s2)
+        counts = [0]*26
+
+        for char in s1:
+            counts[ord(char)-97]+=1
+        
+        for i in range(0,n1):
+            counts[ord(s2[i])-97]-=1
+
+        not_matched = 0
+
+        for count in counts:
+            if count!=0:
+                not_matched+=1
+
+        if not_matched == 0:
+            return True
+
+        for i in range(0,n2-n1):
+            
+            if counts[ord(s2[i])-97] == 0:
+                not_matched += 1
+            elif counts[ord(s2[i])-97] == -1:
+                not_matched -= 1
+            counts[ord(s2[i])-97]+=1
+                
+            counts[ord(s2[i+n1])-97] -= 1
+            if counts[ord(s2[i+len(s1)])-97] == 0:
+                not_matched -= 1
+            elif counts[ord(s2[i+n1])-97] == -1:
+                not_matched += 1
+
+            
+            if not_matched == 0:
+                return True
+        
+        return False
+
