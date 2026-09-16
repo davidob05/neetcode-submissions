77cb474c99629f306b05bb24f9ab0695747aaa15
David O'Brien <156262935+davidob05@users.noreply.github.com>
2026-09-16 14:22:02 +0100

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        queue = deque([])
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==0:
+                    queue.append(tuple([i,j]))
+                    
+
+        while len(queue)>0:
+            elt = queue.popleft()
+            i,j = elt[0],elt[1]
+            if i>0 and (grid[i-1][j] == 2147483647 or grid[i-1][j] > grid[i][j]+1):
+                grid[i-1][j] = grid[i][j] + 1
+                queue.append(tuple([i-1,j]))
+                
+            if i<len(grid) - 1 and (grid[i+1][j] == 2147483647 or grid[i+1][j] > grid[i][j]+1):
+                grid[i+1][j] = grid[i][j] + 1
+                queue.append(tuple([i+1,j]))
+
+            if j>0 and (grid[i][j-1] == 2147483647 or grid[i][j-1] > grid[i][j]+1):
+                grid[i][j-1] = grid[i][j] + 1
+                queue.append(tuple([i,j-1]))
+
+            if j< len(grid[0]) - 1 and (grid[i][j+1] == 2147483647 or grid[i][j+1] > grid[i][j]+1):
+                grid[i][j+1] = grid[i][j] + 1
+                queue.append(tuple([i,j+1]))
+                
+
+                    
+
+
+        
